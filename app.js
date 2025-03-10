6edae70e6c5ee73eb44f91983982f1fcd8b887b3
09div <divya09ag@gmail.com>
2025-03-10 22:19:23 +0530

    backend

---
+++ app.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var passport = require('passport');
+const cors = require('cors')
+const bus=require('./models/Buses')
+
+
+var app = express();
+
+
+// Login and Register 
+require('./auth/auth');
+const login = require('./routes/login')
+const loggedInPage = require('./routes/loggedInUser');
+// ----------------------------------------------------
+
+const bookingRoute = require('./routes/routeSelection')
+
+var registerRouter = require('./routes/register');
+//--------------------------------------------------------
+
+
+//DB Config
+const DB_URL = require('./config/keys').MongoURI;
+
+//connect to mongo
+//---------------------------------------------
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(async() => {
+        console.log("Connected to MongoDB")
+        const buses = await bus.find({ startCity: "Chennai", destination: "Hyderabad" });
+
+        console.log("Buses found:", buses);
+
+    })
+    .catch(err => {
+        throw err
+    })
+//---------------------------------------------
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+app.use('/', login);
+app.use('/booking', bookingRoute);
+app.use('/register', registerRouter);  // To register page 
+app.use('/user', passport.authenticate('jwt', { session: false }), loggedInPage); //To Secure Route
+
+module.exports = app;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: "mongodb+srv://divya65air:AhiiBhQjmtL9cCd1@cluster0.easo9.mongodb.net/test?retryWrites=true&w=majority"
+}
